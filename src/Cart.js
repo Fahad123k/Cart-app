b12ddb16d22e1de1d17e9df0b8f894c851cfd453
Fahad123k <ahmad.nishu08@gmail.com>
2022-08-13 11:26:17 +0530

    Handle cart increase/decrease

--- src/Cart.js
+++ src/Cart.js
@@ -13,14 +13,16 @@ class Cart extends React.Component {
                     price:39999,
                     title:"Iphone",
                     qty:1,
-                    Cartimage:'https://cdn-icons-png.flaticon.com/512/644/644458.png'
+                    Cartimage:'https://cdn-icons-png.flaticon.com/512/644/644458.png',
+                    id:1
                 },
                 {
                     
                         price:5999,
                         title:"Watch",
                         qty:2,
-                        Cartimage:'https://as1.ftcdn.net/v2/jpg/03/01/52/70/1000_F_301527093_MsrLRU3oL6lGoF5OCBNGugZ1M4MkZzXm.jpg'
+                        Cartimage:'https://as1.ftcdn.net/v2/jpg/03/01/52/70/1000_F_301527093_MsrLRU3oL6lGoF5OCBNGugZ1M4MkZzXm.jpg',
+                        id:2
                     
                 },
                 {
@@ -28,12 +30,47 @@ class Cart extends React.Component {
                     price:6999,
                     title:"laptop",
                     qty:2,
-                    Cartimage:'https://as1.ftcdn.net/v2/jpg/00/86/83/68/1000_F_86836872_WRScBX15NPeRRWjfzRZgkuzQCZZlpSzB.jpg'                
+                    Cartimage:'https://as1.ftcdn.net/v2/jpg/00/86/83/68/1000_F_86836872_WRScBX15NPeRRWjfzRZgkuzQCZZlpSzB.jpg',
+                    id:3            
             }
             ]
         }
     }
 
+handleIncreaseQuantity=(product)=>{
+    // console.log('hey increase qty',product);
+    const {products}=this.state;
+    const index = products.indexOf(product);
+    products[index].qty+=1;
+    // console.log( products[index].qty)
+    const max= products[index].qty;
+
+    if(max<=5){
+        this.setState({
+            products
+        })
+    }
+    else{
+        window.alert("cant add more than 5 products")
+    }
+   
+}
+handleDecreaseQuantity=(product)=>{
+    // console.log('hey decrease qty',product);
+    const {products}=this.state;
+    const index = products.indexOf(product);
+    products[index].qty-=1;
+    const min= products[index].qty;
+    if(min>0){
+        this.setState({
+            products
+    
+        })
+    }
+    else{
+        window.alert("Hey Your cart item will be empty")
+    }
+}
 
   render () {
    const {products}=this.state;
@@ -41,7 +78,14 @@ class Cart extends React.Component {
       <div className="cart">
         {
             products.map((product)=>{
-                return <CartItems product={product}/>
+                return (
+                    <CartItems 
+                    product={product} 
+                    key={product.id}
+                    increaseQuantity={this.handleIncreaseQuantity}
+                    decreaseQuantity={this.handleDecreaseQuantity}
+                    />
+                )
             })
         }
        
--- src/CartItems.js
+++ src/CartItems.js
@@ -2,48 +2,48 @@ import React from "react";
 
 class CartItems extends React.Component{
   
-    increaseQuantity=()=>{
-        console.log("increasing test",this)
+    // increaseQuantity=()=>{
+    //     console.log("increasing test",this)
         
-        if(this.state.qty<5){
-            this.setState({
-                qty: this.state.qty+1
-             })
-        } 
-        else{
-            console.log("cant buy more than 20");
-            window.alert("cant buy more than 10 product")
-        }
+    //     if(this.state.qty<5){
+    //         this.setState({
+    //             qty: this.state.qty+1
+    //          })
+    //     } 
+    //     else{
+    //         console.log("cant buy more than 20");
+    //         window.alert("cant buy more than 10 product")
+    //     }
        
-    }
-    decreaseQuantity=()=>{
-        console.log("decreasing test",this)
-        // second form of changing state
-        // if we require prev sate then we use prevState funct arrow=>
-        if(this.state.qty>1){
-            this.setState((prevState)=>{
-                return{
-                    qty:prevState.qty-1
-                }
-            })
-        }
-        else{
-            window.confirm("Do you not want to buy!")
-        }
+    // }
+    // decreaseQuantity=()=>{
+    //     console.log("decreasing test",this)
+    //     // second form of changing state
+    //     // if we require prev sate then we use prevState funct arrow=>
+    //     if(this.state.qty>1){
+    //         this.setState((prevState)=>{
+    //             return{
+    //                 qty:prevState.qty-1
+    //             }
+    //         })
+    //     }
+    //     else{
+    //         window.confirm("Do you not want to buy!")
+    //     }
        
-    }
+    // }
 
-    removeCartItems=()=>{
-        console.log("increasing test",this.state.qty)
+    // removeCartItems=()=>{
+    //     console.log("increasing test",this.state.qty)
         
-        window.alert("Are you sure to delete cart items!")
-        this.setState({
+    //     window.alert("Are you sure to delete cart items!")
+    //     this.setState({
         
-            qty: this.state.qty=0
-         })
+    //         qty: this.state.qty=0
+    //      })
        
        
-    }
+    // }
     render(){
         console.log('this.props',this.props)
         const {price,title,qty, Cartimage}=this.props.product;
@@ -60,13 +60,13 @@ class CartItems extends React.Component{
                         {/* buttons */}
                         {/* htttp://www.flatIcons.com */}
                         <img src="https://as2.ftcdn.net/v2/jpg/01/26/10/59/1000_F_126105961_6vHCTRX2cPOnQTBvx9OSAwRUapYTEmYA.jpg" alt="increase" className="action-icons" 
-                        onClick={this.increaseQuantity.bind(this)}
+                        onClick={()=> this.props.increaseQuantity(this.props.product)}
                         />
                         <img src="https://cdn-icons-png.flaticon.com/512/56/56889.png" alt="decrease" className="action-icons"
-                          onClick={this.decreaseQuantity.bind(this)}
+                         onClick={()=> this.props.decreaseQuantity(this.props.product)}
                         />
                         <img src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" alt="delete" className="action-icons" 
-                        onClick={this.removeCartItems.bind(this)}
+                        
                         />
                     </div>
                 </div>
